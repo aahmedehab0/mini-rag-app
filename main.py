e0d845a329e82493d2a10c367b5ffe4036ab694d
Ahmed Ehab <aahmedehab0@gmail.com>
2025-10-29 14:37:33 +0300

    update with appversion and app name in fastapi drom .env

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv('.env')
+
+from routes import base
+
+
+
 app = FastAPI()
+app.include_router(base.base_router)
 
-@app.get("/welcome")
-def welcome():
-    return {
-        "message" : "Hello From Fast Api"
-    }
 
-#uvicorn main:app --reload  --host 0.0.0.0 --port 5000
+
+#
 # run fastapi from main.py decoder app with reload (refresh directly)
 #  any one can access run on port 5000
