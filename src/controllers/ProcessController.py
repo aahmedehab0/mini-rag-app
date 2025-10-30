ead503709d47ac0f23cfd6317f2b536ff5ace0c7
Ahmed Ehab <aahmedehab0@gmail.com>
2025-10-30 18:42:41 +0300

    make fastapi cut files into chunks

--- src/controllers/ProcessController.py
+++ src/controllers/ProcessController.py
@@ -1,15 +1,62 @@
 from .BaseController import BaseController
 from .ProjectController import ProjectController
+from models import ProcessingEnum
+from langchain_community.document_loaders import  TextLoader
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 import os
 
 class ProcessController(BaseController):
     def __init__(self , project_id : str):
         super().__init__()
         self.project_id = project_id
-        self.project_path = ProjectController.get_project_path(project_id=project_id)
+        self.project_path = ProjectController().get_project_path(project_id=project_id)
 
-    def get_file_extension(self , file_id):
-
+    def get_file_extension(self , file_id : str ):
         return os.path.splitext(file_id)[-1]
 
+    def get_file_loader(self , file_id : str ):
+        file_path = os.path.join(self.project_path ,
+                                 file_id
+                                 )
+        file_ext = self.get_file_extension (file_id = file_id)
+
+        if file_ext == ProcessingEnum.TXT.value:
+            return TextLoader (file_path ,encoding= "utf-8")
+        
+        if file_ext == ProcessingEnum.PDF.value:
+            return PyMuPDFLoader (file_path , encoding= "utf-8")
+        
+        return None
     
+    def get_file_content(self , file_id:str):
+        loader = self.get_file_loader(file_id = file_id)
+        return loader.load()
+    
+    def process_file_content (self ,file_contents : list , file_id :str,
+                              chunk_size : int=100 , overlap_size : int=20 
+    ): 
+        
+        text_spliter = RecursiveCharacterTextSplitter(
+            chunk_size = chunk_size,
+            chunk_overlap = overlap_size,
+            length_function = len
+        )
+
+        file_content_texts = [ 
+            rec.page_content for  
+            rec in file_contents
+        ]
+
+        file_metadata_texts = [ 
+            rec.metadata for  
+            rec in file_contents
+        ]
+
+        chunks = text_spliter.create_documents(
+            texts= file_content_texts,
+            metadatas= file_metadata_texts
+        )
+        return chunks
+        
--- src/routes/data.py
+++ src/routes/data.py
@@ -1,7 +1,7 @@
 from fastapi import  APIRouter, Depends, UploadFile, status
 from fastapi.responses import JSONResponse
 from helpers.confog import get_setings ,Settings
-from controllers import DataController
+from controllers import DataController,ProcessController
 import aiofiles
 from models import ResponseSignal
 import logging
@@ -64,4 +64,25 @@ async def upload_data(project_id: str, file: UploadFile,
 async def process_endpoint (project_id: str , process_request :ProcessRequest ):
 
     file_id = process_request.file_id
-    return file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+
+    process_controller = ProcessController(project_id = project_id)
+
+    file_content = process_controller.get_file_content (file_id = file_id)
+
+    file_chuncks = process_controller.process_file_content( file_contents =file_content,
+                                                       file_id = file_id,
+                                                       chunk_size = chunk_size,
+                                                       overlap_size =overlap_size )
+    
+    if file_chuncks is None or len (file_chuncks) ==0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "signal": ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    return  file_chuncks
+
+
