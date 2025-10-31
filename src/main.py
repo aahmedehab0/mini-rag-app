1bb5fd029fb1be5ce05818b2c7ea0304da279ef0
Ahmed Ehab <aahmedehab0@gmail.com>
2025-10-31 14:51:03 +0200

    add async contex manger for start and shit down mongo db

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI 
 from routes import base , data
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers import get_setings
+from contextlib import asynccontextmanager
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    settings = get_setings()
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)  #define clent
+    app.db_clent = app.mongo_conn [settings.MONGODB_DATABASE]  #define database
+    yield
+    app.mongo_conn.close()
 
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan )
+
+
 app.include_router(base.base_router)
 app.include_router (data.data_router)
 
