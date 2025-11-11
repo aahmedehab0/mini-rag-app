f46c822d086cd9d9f63de80b7d893b1d90cb80b9
Ahmed Ehab <aahmedehab0@gmail.com>
2025-11-11 20:23:37 +0200

    now model work with upload file and get chunks and embeding it in vector database and search similarity from vector database and query then answer it by adding some prompts with generation model

--- src/main.py
+++ src/main.py
@@ -54,6 +54,7 @@ app.include_router (data.data_router)
 app.include_router(nlp.nlp_router)
 
 
+
 #uvicorn main:app --reload  --host 0.0.0.0 --port 5000
 # run fastapi from main.py decoder app with reload (refresh directly)
 #  any one can access run on port 5000
